2631921620aebb6f04c6dcb10952ac6546466387
Stephen-Heff <folosstephen@gmail.com>
2023-05-19 11:36:26 -0400

    updated edcation to show images

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -15,14 +15,15 @@ export default function Education() {
   return (
     <section class="education content-wrapper" id="education">
       <h2>My Education</h2>
-        <ul  >
-          {education.map((education) => (
-            <li key={education.id} className="border">
-              <h3>{education.title}</h3>
-              <p>{education.description}</p>
-            </li>
-          ))}
-        </ul>
+      <ul>
+        {education.map((education) => (
+          <li key={education.id} className="border">
+            <img src={education.image} />
+            <h3>{education.title}</h3>
+            <p>{education.description}</p>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
